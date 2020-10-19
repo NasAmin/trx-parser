7c646ae838f9cc632c38103e840ae7bc09fba941
Nasir Amin <nasir_amin@waters.com>
2020-10-19 15:08:11 +0100

    Set trx path to workspace

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ export async function run(): Promise<void> {
     core.setOutput('test-outcome', 'Passed')
     core.setOutput('trx-path', trxPath)
 
-    const trxFiles = getTrxFiles('.')
+    const trxFiles = getTrxFiles(trxPath)
 
     core.setOutput('trx-files', trxFiles)
 
