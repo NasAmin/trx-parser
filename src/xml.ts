20cf9fa3ca3d4bbb1f6cf1a114d5e91592f0ebd5
Nasir Amin <nasir_amin@waters.com>
2020-10-19 15:18:25 +0100

    Simulate a change

--- src/xml.ts
+++ src/xml.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs'
 export async function getTrxFiles(path: string) : Promise<string>{
-    fs.readdir('.', (err, files) => {
+    fs.readdir(path, (err, files) => {
         files.forEach(file => {
           console.log(file);
         });
