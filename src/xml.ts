f57e2d274cc661b0282ff51e9a217fbbdc0ac2d2
Nasir Amin <nasir_amin@waters.com>
2020-10-20 09:36:10 +0100

    Fixed lint issues

--- src/xml.ts
+++ src/xml.ts
@@ -1,14 +1,15 @@
 import * as fs from 'fs'
 import * as path from 'path'
-
 import * as uitl from 'util'
+import * as core from '@actions/core'
 
 export async function getTrxFiles(trxPath: string): Promise<string[]> {
-  // TODO: Convert to async version    
-  const files = fs.readdirSync(path.resolve(trxPath), {withFileTypes: true})  
+  // TODO: Convert to async version
+  const files = fs.readdirSync(path.resolve(trxPath), {withFileTypes: true})
   const readdir = uitl.promisify(fs.readdir)
-  let fileNames = (await readdir(trxPath))
-  let trxFiles = fileNames.filter(f => f.endsWith('.json'))
-  
+  const fileNames = await readdir(trxPath)
+  const trxFiles = fileNames.filter(f => f.endsWith('.json'))
+  core.info(`Files count: ${files.length}`)
+
   return trxFiles
 }
