372487f7fbf7e2eedb7c9e70a51d30aabbe4e671
Nasir Amin <nasir_amin@waters.com>
2020-10-21 15:02:26 +0100

    Added error handling if the action fails to create a check

--- src/github.ts
+++ src/github.ts
@@ -4,7 +4,7 @@ import * as core from '@actions/core'
 export async function createCheckRun(repoToken: string): Promise<void> {
   if (github.context.eventName === 'pullRequest') {
     const octokit = github.getOctokit(repoToken)
-    octokit.checks.create({
+    const response = await octokit.checks.create({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       name: 'trx-parser',
@@ -17,6 +17,11 @@ export async function createCheckRun(repoToken: string): Promise<void> {
         text: 'test'
       }
     })
+    if (response.status !== 201) {
+      throw new Error(
+        `Failed to create status check. Error code: ${response.status}`
+      )
+    }
   } else {
     core.info('Skipping status check as the trigger was not on a pull request')
   }
