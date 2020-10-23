3b21ce3d2105eb3dcfe2897c23afb894103c62ab
Nasir Amin <nasir_amin@waters.com>
2020-10-23 21:23:51 +0100

    Fixed ps paths

--- trx-reports/sample-test-results.ps1
+++ trx-reports/sample-test-results.ps1
@@ -14,8 +14,8 @@ function Build-MarkdownReport {
     $test_results_path = "$PSScriptRoot/sample-test-results.trx"
     $test_report_path = "$PSScriptRoot/sample-test-results.md"    
     & "$PSScriptRoot/trx2md.ps1" -Verbose `
-        -trxFile $script:test_results_path `
-        -mdFile $script:test_report_path -xslParams @{
+        -trxFile $test_results_path `
+        -mdFile $test_report_path -xslParams @{
             reportTitle = $script:report_title
         }
 }
