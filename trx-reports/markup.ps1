794fd80f1988e295aefad7105d43bd5dd71cf81d
Nasir Amin <nasir_amin@waters.com>
2020-10-26 10:27:33 +0000

    Print powershell paarams for debugging

--- trx-reports/markup.ps1
+++ trx-reports/markup.ps1
@@ -1,4 +1,19 @@
 #!/usr/bin/env pwsh
+
+## You interface with the Actions/Workflow system by interacting
+## with the environment.  The `GitHubActions` module makes this
+## easier and more natural by wrapping up access to the Workflow
+## environment in PowerShell-friendly constructions and idioms
+if (-not (Get-Module -ListAvailable GitHubActions)) {
+    ## Make sure the GH Actions module is installed from the Gallery
+    Install-Module GitHubActions -Force
+}
+
+## Load up some common functionality for interacting
+## with the GitHub Actions/Workflow environment
+Import-Module GitHubActions
+
+
 param(    
     [string]$reportName=$null,
     [string]$reportTitle=$null,
@@ -11,6 +26,11 @@ function Build-MarkdownReport {
     $script:report_name = $reportName
     $script:report_title = $reportTitle
 
+    Write-ActionInfo "Report Name is: $reportName"
+    Write-ActionInfo "Report Title is: $reportTitle"
+    Write-ActionInfo "Trx Path is: $trxPath"
+    Write-ActionInfo "Markup Path is: $markupPath"
+
     if (-not $script:report_name) {
         $script:report_name = "TEST_RESULTS_$([datetime]::Now.ToString('yyyyMMdd_hhmmss'))"
     }
@@ -18,7 +38,8 @@ function Build-MarkdownReport {
         $script:report_title = $report_name
     }
 
-    "$PSScriptRoot/trx2md.ps1" -Verbose `
+    $script:test_report_path = Join-Path $tmpDir test-results.md
+    & "$PSScriptRoot/trx2md.ps1" -Verbose `
         -trxFile $trxPath `
         -mdFile $markupPath -xslParams @{
             reportTitle = $script:report_title
