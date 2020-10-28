fa7e0c0fe7aa110173a824eeca70c1e9eb976f46
Nasir Amin <nasir_amin@waters.com>
2020-10-28 09:58:00 +0000

    Set check completion time as a markup using a backtick

--- src/github.ts
+++ src/github.ts
@@ -22,7 +22,8 @@ export async function createCheckRun(
       const markupData = await getMarkupForTrxFromGist(
         reportData.ReportMetaData.MarkupFilePath
       )
-      const checkTime = new Date().toUTCString()
+      let checkTime = new Date().toUTCString()
+      checkTime = `\`${checkTime}\``
       const response = await octokit.checks.create({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
@@ -35,7 +36,7 @@ export async function createCheckRun(
             : 'success',
         output: {
           title: reportData.ReportMetaData.ReportTitle,
-          summary: `This test run completed at \`${checkTime}\``,
+          summary: `This test run completed at ${checkTime}`,
           // text: reportData.ReportMetaData.TrxJSonString
           text: markupData
         }
