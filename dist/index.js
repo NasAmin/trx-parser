4d91a0c542d4e9360fe164e91b5f407d406b7b9c
Nasir Amin <nasir_amin@waters.com>
2020-10-28 13:09:19 +0000

    Added logging again

--- dist/index.js
+++ dist/index.js
@@ -54,6 +54,7 @@ function createCheckRun(repoToken, reportData) {
                 core.info(`Creating status check for GitSha: ${git_sha}`);
                 const markupData = yield markup_1.getMarkupForTrxFromGist(reportData.ReportMetaData.MarkupFilePath);
                 const checkTime = new Date().toUTCString();
+                core.info(`Check time is: ${checkTime}`);
                 const response = yield octokit.checks.create({
                     owner: github.context.repo.owner,
                     repo: github.context.repo.repo,
