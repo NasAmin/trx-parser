13c7699b61e4f5679da5484ad413a0c4fd219c66
Nasir Amin <nasir_amin@waters.com>
2020-10-30 13:54:16 +0000

    Create report header

--- src/markup.ts
+++ src/markup.ts
@@ -3,21 +3,36 @@ import {promises} from 'fs'
 import {TrxDataWrapper} from './types/types'
 
 export function getMarkupForTrx(testData: TrxDataWrapper): string {
+  const startTime = new Date(testData.TrxData.TestRun.Times._start).getSeconds()
+  const endTime = new Date(testData.TrxData.TestRun.Times._finish).getSeconds()
+  const duration = startTime - endTime
   return `
 # Test Results - ${testData.ReportMetaData.ReportTitle}
 <p>Expand the following summaries for more details:</p>
-<details>
-  <summary> Duration: blah seconds </summary>
+<details>  
+  <summary> Duration: ${duration} seconds </summary>
   <table>
     <tr>
-       <th>Started</th>
-       <td>heh</td>  
+       <th>Started:</th>
+       <td>\`${testData.TrxData.TestRun.Times._start}\`</td>
     </tr>
     <tr>
-       <th>Allowed Statuses</th>
-       <td>blah</td>
-       <td>:heavy_check_mark:</td>
-     </tr>
+       <th>Creation:</th>
+       <td>\`${testData.TrxData.TestRun.Times._finish}\`</td>
+    </tr>
+    <tr>
+       <th>Queuing:</th>
+       <td>\`${testData.TrxData.TestRun.Times._queuing}\`</td>
+    </tr>
+    <tr>
+       <th>Finished:</th>
+       <td>\`${testData.TrxData.TestRun.Times._finish}\`</td>    
+    </tr>
+    <tr>
+       <th>Duration:</th>
+       <td>\`${duration}\`</td>
+    </tr>
+
   </table>
 </details>
 `
