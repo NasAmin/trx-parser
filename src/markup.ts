05a0acda61c6c98cdd52b4ccab5e4ea15448f9ef
Nasir Amin <nasir_amin@waters.com>
2020-10-30 14:27:20 +0000

    Added markup for test results summary

--- src/markup.ts
+++ src/markup.ts
@@ -22,7 +22,7 @@ export function getMarkupForTrx(testData: TrxDataWrapper): string {
     </tr>
     <tr>
        <th>Queuing:</th>
-       <td>\`\`\`${testData.TrxData.TestRun.Times._queuing}\`\`\`</td>
+       <td>${testData.TrxData.TestRun.Times._queuing}</td>
     </tr>
     <tr>
        <th>Finished:</th>
@@ -30,9 +30,78 @@ export function getMarkupForTrx(testData: TrxDataWrapper): string {
     </tr>
     <tr>
        <th>Duration:</th>
-       <td>${duration}</td>
+       <td>${duration} seconds</td>
     </tr>
 
+  </table>
+</details>
+<details>  
+  <summary> Outcome: ${testData.TrxData.TestRun.ResultSummary._outcome} | Total Tests: ${testData.TrxData.TestRun.ResultSummary.Counters._total} | Failed: ${testData.TrxData.TestRun.ResultSummary.Counters._failed} </summary>
+  <table>
+    <tr>
+       <th>Total:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._total}</td>
+    </tr>
+    <tr>
+       <th>Executed:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._executed}</td>
+    </tr>
+    <tr>
+       <th>Passed:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._passed}</td>
+    </tr>
+    <tr>
+       <th>Failed:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._failed}</td>    
+    </tr>
+    <tr>
+       <th>Error:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._error}</td>
+    </tr>
+    <tr>
+       <th>Timeout:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._timeout}</td>
+    </tr>
+    <tr>
+       <th>Aborted:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._aborted}</td>
+    </tr>
+    <tr>
+       <th>Inconclusive:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._inconclusive}</td>
+    </tr>
+    <tr>
+       <th>PassedButRunAborted:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._passedButRunAborted}</td>
+    </tr>
+    <tr>
+       <th>NotRunnable:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._notRunnable}</td>
+    </tr>
+    <tr>
+       <th>NotExecuted:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._notExecuted}</td>
+    </tr>
+    <tr>
+       <th>Disconnected:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._disconnected}</td>
+    </tr>
+    <tr>
+       <th>Warning:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._warning}</td>
+    </tr>
+    <tr>
+       <th>Completed:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._completed}</td>
+    </tr>
+    <tr>
+       <th>InProgress:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._inProgress}</td>
+    </tr>
+    <tr>
+       <th>Pending:</th>
+       <td>${testData.TrxData.TestRun.ResultSummary.Counters._pending}</td>
+    </tr>
   </table>
 </details>
 `
