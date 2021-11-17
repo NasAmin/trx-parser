74fb16cf9cbb2208ebb623a1750dd322d984355f
Andrey Klochaniy <Andrey.klochaniy@gmail.com>
2021-11-17 14:08:07 +0200

    Update github.ts

--- src/github.ts
+++ src/github.ts
@@ -52,7 +52,7 @@ export async function createCheckRun(
           : 'success',
       output: {
         title: reportData.ReportMetaData.ReportTitle,
-        summary: `This test run completed at \`${checkTime}\``,
+        summary: `My own; This test run completed at \`${checkTime}\``,
         // text: reportData.ReportMetaData.TrxJSonString
         text: markupData
       }
