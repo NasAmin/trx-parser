b03e46083603f2a1169533df9530a3180474de15
Nasir Amin <nasir.amin@outlook.com>
2023-08-19 20:23:50 +0100

    experiment

--- __tests__/markup.test.ts
+++ __tests__/markup.test.ts
@@ -27,6 +27,32 @@ describe('When generating markup for trx', () => {
     // console.log(testData)
   })
 
+  describe('Test failing tests with stack trace', () => {
+    test('LoadXml Should have an outcome of Failed()', async () => {
+      const data = await transformTrxToJson(
+        './test-data/failing-tests/stack-trace.trx'
+      )
+      const testData = getMarkupForTrx(data)
+      expect(data.TrxData.TestRun.ResultSummary._outcome).toEqual('Failed')
+      expect(testData).toContain(
+        `Test Results - ${data.ReportMetaData.ReportTitle}`
+      )
+      expect(testData).toContain(
+        `Total Tests: ${data.TrxData.TestRun.ResultSummary.Counters._total}`
+      )
+      expect(testData).toContain(
+        `Failed: ${data.TrxData.TestRun.ResultSummary.Counters._failed}`
+      )
+
+      expect(testData).toContain(data.ReportMetaData.ReportTitle)
+
+      expect(testData).toContain(data.TrxData.TestRun.Times._start)
+      expect(testData).toContain(data.TrxData.TestRun.Times._finish)
+
+      // console.log(testData)
+    })
+  })
+
   test('LoadXml For single test()', async () => {
     const data = await transformTrxToJson(
       './test-data/passing-tests/single-test.trx'
